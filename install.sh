77fd921d1ec6e399a77294f95c681ef27e157de0
Donato Borrello <venar303@gmail.com>
2014-03-28 00:39:49 -0400

    Adding vim stuff

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ## Sometimes necessary (for backtrack 5)
 #echo "nameserver 8.8.8.8" > /etc/resolv.conf
+mkdir ~/local
 
 #Basic updating
 apt-get -y update
@@ -11,26 +12,44 @@ apt-get -y upgrade
 apt-get -y install lftp
 
 #web stuff
-apt-get -y install apache2 php5 mysql
+apt-get -y install apache2 php5
 service -y apache2 restart
 
-#development stuff
-apt-get -y install git ruby python nodejs vim
 
+# We like building things right?
+apt-get -y install build-essential
+
+#Required by phantom-js and maybe some other things
+apt-get -y install Freetype
 
 # VIM plugins and settings
+#install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+#Add some necessary libaries
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-sensible.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/jelera/vim-javascript-syntax.git
+git clone https://github.com/othree/javascript-libraries-syntax.vim.git
+git clone https://github.com/vim-scripts/SyntaxComplete.git
+git clone https://github.com/mbbill/undotree.git
 
-
-# setup .bashrc
-
+# Source in our personal .bashrc 
+echo "source ~/saneunix/.bashrc" >> ~/.bashrc
 
 #setup PHP error reporting 
 #    vim /etc/php5/apache2/php.ini
 #    vim /etc/php5/apache2/php.ini
 #    * search "display_errors" and "error_reporting"
     
-    
+
 # come back to this...
 #cat ~/.lftp/rc
 #set ssl:verify-certificate no
 
+
+#Basic updating
+apt-get -y update
+apt-get -y upgrade
