544c7ce02c654ee50e8b5d9a3e27bcb32a27fc08
Donato Borrello <venar303@gmail.com>
2014-03-28 11:16:47 -0400

    Better organization, some new functionality
    Now will be creating a default .vimrc which sources our version.
    Adding codes to install the LAMP stack, and stubbed out some future
    enhancements with comments

--- install.sh
+++ install.sh
@@ -2,52 +2,78 @@
 
 ## Sometimes necessary (for backtrack 5)
 #echo "nameserver 8.8.8.8" > /etc/resolv.conf
-mkdir ~/local
+mkdir -p ~/local ~/projects
 
 #Basic updating
 apt-get -y update
 apt-get -y upgrade
 
-#ftp service
-apt-get -y install lftp
 
-#web stuff
-apt-get -y install apache2 php5
-service -y apache2 restart
+#change SSH port, and setup SSH keys
+
+#lockdown other ports
+
+
+
+
+## FTP
+    apt-get -y install lftp
+
+    # come back to this...
+    #cat ~/.lftp/rc
+    #set ssl:verify-certificate no
 
 
 # We like building things right?
 apt-get -y install build-essential
 
-#Required by phantom-js and maybe some other things
-apt-get -y install Freetype
+## VIM
+    #Add pathogen
+    mkdir -p ~/.vim/autoload ~/.vim/bundle
+    curl -Sso ~/.vim/autoload/pathogen.vim \
+        https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-# VIM plugins and settings
-#install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim \
-    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-#Add some necessary libaries
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-sensible.git
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/jelera/vim-javascript-syntax.git
-git clone https://github.com/othree/javascript-libraries-syntax.vim.git
-git clone https://github.com/vim-scripts/SyntaxComplete.git
-git clone https://github.com/mbbill/undotree.git
+    #Add some necessary libaries
+    cd ~/.vim/bundle
+    git clone git://github.com/tpope/vim-sensible.git
+    git clone https://github.com/kien/ctrlp.vim.git
+    git clone https://github.com/jelera/vim-javascript-syntax.git
+    git clone https://github.com/othree/javascript-libraries-syntax.vim.git
+    git clone https://github.com/vim-scripts/SyntaxComplete.git
+    git clone https://github.com/mbbill/undotree.git
 
-# Source in our personal .bashrc 
-echo "source ~/saneunix/.bashrc" >> ~/.bashrc
-
-#setup PHP error reporting 
-#    vim /etc/php5/apache2/php.ini
-#    vim /etc/php5/apache2/php.ini
-#    * search "display_errors" and "error_reporting"
+    #Now setup the .vimrc
+    rm -f ~/.vimrc
+    echo ":so ~/saneunix/.vimrc" >> ~/.vimrc
     
+## TERMINAL    
+    # Source in our personal .bashrc 
+    echo "source ~/saneunix/.bashrc" >> ~/.bashrc
 
-# come back to this...
-#cat ~/.lftp/rc
-#set ssl:verify-certificate no
+    
+    
+## LAMP STACK
+    #Apache
+    apt-get -y install apache2 
+    #mysql
+    apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+    #php
+    apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+    
+    #final setup
+    sudo service -y apache2 restart
+    #sudo mysql_install_db
+    #sudo /usr/bin/mysql_secure_installation
+
+    #setup PHP error reporting 
+    #    vim /etc/php5/apache2/php.ini
+    #    vim /etc/php5/apache2/php.ini
+    #    * search "display_errors" and "error_reporting"
+        
+    #set up new relic monitoring?
+
+    #set up automysqlbackup?
+
 
 
 #Basic updating
--- install_phantomjs_1.9.sh
+++ install_phantomjs_1.9.sh
@@ -4,7 +4,7 @@ mkdir ~/local
 cd ~/local
 
 sudo -y apt-get update
-sudo -y apt-get install build-essential chrpath git-core libssl-dev libfontconfig1-dev
+sudo -y apt-get install Freetype build-essential chrpath git-core libssl-dev libfontconfig1-dev
 git clone git://github.com/ariya/phantomjs.git
 cd phantomjs
 git checkout 1.9
